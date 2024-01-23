cc09a103abd306b7b68df400aa46464a62ef233c
Halomoan K <halomoan@gmail.com>
2024-01-23 23:12:14 +0800

    home commit

--- webapp/pages/controller/DetermineAgentEntry.controller.js
+++ webapp/pages/controller/DetermineAgentEntry.controller.js
@@ -1,10 +1,33 @@
 sap.ui.define([
 	"zwfuol/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
 ], function(
-	BaseController
+	BaseController,
+	JSONModel
 ) {
 	"use strict";
 
 	return BaseController.extend("zwfuol.pages.controller.DetermineAgentEntry", {
+		/**
+		 * @override
+		 */
+		onInit: function() {
+			var oViewModel = new JSONModel({
+				showSave: false,
+				showDelete: false,
+				RuleOrderSet: []
+			});
+
+			var aRuleOrder = [];
+
+			for(var i = 0; i<50 ; i++){
+				var idx = "" + i;
+				aRuleOrder.push(idx.padStart(3,"0"));
+			}
+
+			var oView = this.getView();
+			oView.setModel(oViewModel, "viewData");
+
+		}
 	});
 });
